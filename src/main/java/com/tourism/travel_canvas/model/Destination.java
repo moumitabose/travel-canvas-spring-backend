2f660661a44cbd4ea3c431d3a0be615cc738eb18
Moumita Bose <ibosemoumita@gmail.com>
2024-11-25 17:31:10 +0530

    update Destination POJO

--- src/main/java/com/tourism/travel_canvas/model/Destination.java
+++ src/main/java/com/tourism/travel_canvas/model/Destination.java
@@ -1,8 +1,12 @@
 package com.tourism.travel_canvas.model;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 @Table(schema = "tourism", name = "destination")
@@ -10,19 +14,83 @@ public class Destination {
 	
 	@Id
 	private Integer destinationid;
-	private String country;
+	private Integer cityid;
+	private Integer countryid;
+	private Character activeflag;
+	private String createby;
+
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+	private Date createdt;
+
+	private String modby;
+
+	@JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+	private Date moddt;
+
 	public Integer getDestinationid() {
 		return destinationid;
 	}
+
 	public void setDestinationid(Integer destinationid) {
 		this.destinationid = destinationid;
 	}
-	public String getCountry() {
-		return country;
+
+	public Integer getCityid() {
+		return cityid;
 	}
-	public void setCountry(String country) {
-		this.country = country;
+
+	public void setCityid(Integer cityid) {
+		this.cityid = cityid;
 	}
+
+	public Integer getCountryid() {
+		return countryid;
+	}
+
+	public void setCountryid(Integer countryid) {
+		this.countryid = countryid;
+	}
+
+	public Character getActiveflag() {
+		return activeflag;
+	}
+
+	public void setActiveflag(Character activeflag) {
+		this.activeflag = activeflag;
+	}
+
+	public String getCreateby() {
+		return createby;
+	}
+
+	public void setCreateby(String createby) {
+		this.createby = createby;
+	}
+
+	public Date getCreatedt() {
+		return createdt;
+	}
+
+	public void setCreatedt(Date createdt) {
+		this.createdt = createdt;
+	}
+
+	public String getModby() {
+		return modby;
+	}
+
+	public void setModby(String modby) {
+		this.modby = modby;
+	}
+
+	public Date getModdt() {
+		return moddt;
+	}
+
+	public void setModdt(Date moddt) {
+		this.moddt = moddt;
+	}
+
 	
 	
 	
@@ -30,5 +98,6 @@ public class Destination {
 	
 	
 	
+
 
 }
